6e9f5d71450b0668332bee8d9a4a5ce36ab1bf1b
AleksaMCode <aleksamcode@gmail.com>
2023-02-24 19:05:16 +0100

    RC4 updated RC4-drop[n] implemented Output format updated to print every character combined instead of individual hex characters Unnecessary imports removed

--- rc4.py
+++ rc4.py
@@ -1,7 +1,3 @@
-from lib2to3.pytree import convert
-from re import L
-
-
 def key_scheduling_algorithm(key):
     # Array "S" is initialized to the identity permutation
     S = list(range(256))
@@ -19,27 +15,28 @@ def key_scheduling_algorithm(key):
     return S
 
 
-def pseudo_random_generation_algorithm(S, data):
+def pseudo_random_generation_algorithm(S, data, n=0):
     result = [0] * len(data)
 
     i = 0
     j = 0
     s_length = len(S)
-    for iteration in range(len(data)):
+    for iteration in range(len(data) + n):
         # increment i
         i = (i + 1) % s_length
 
         # looks up the ith element of S, S[i], and adds that to j
         j = (j + S[i]) % s_length
 
-        # exchanges the values of S[i] and S[j] then uses the sum S[i] + S[j] (modulo 256) as an index to fetch a
-        # third element of S (the keystream value K below)
+        # exchanges the values of S[i] and S[j]
         S[i], S[j] = S[j], S[i]
-        k = S[(S[i] + S[j]) % s_length]
 
-        # then bitwise exclusive ORed (XORed) with the next byte of the message to produce the next byte of either
-        # ciphertext or plaintext.
-        result[iteration] = ord(data[iteration]) ^ k
+        if iteration >= n:
+            # use the modular sum S[i] + S[j] as an index to fetch a third element of S
+            k = S[(S[i] + S[j]) % s_length]
+            # then bitwise exclusive ORed (XORed) with the next byte of the message to produce the next byte of either
+            # ciphertext or plaintext.
+            result[iteration - n] = ord(data[iteration - n]) ^ k
 
     return result
 
@@ -48,8 +45,8 @@ def convert_char_to_int(value):
     return [ord(ch) for ch in value]
 
 
-def rc4_encrypt(key, plaintext):
-    return pseudo_random_generation_algorithm(key_scheduling_algorithm(key), plaintext)
+def rc4_encrypt(key, plaintext, n=0):
+    return pseudo_random_generation_algorithm(key_scheduling_algorithm(key), plaintext, n)
 
 
 def rc4_dencrypt(key, plaintext):
@@ -64,7 +61,7 @@ def main():
 
     print("Ciphertext: ", end="")
     for el in result:
-        print(hex(el), end=" ")
+        print(f"{el:X}", end="")
 
 
 if __name__ == "__main__":
